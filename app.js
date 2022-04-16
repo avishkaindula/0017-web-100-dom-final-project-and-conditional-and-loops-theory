bc645a8eb53058acde6c6a6a3d3ad1c0237e7794
Avishka Indula <avishkainduladev@gmail.com>
2022-04-17 00:18:32 +0530

    style , className and classList of DOM

--- app.js
+++ app.js
@@ -16,3 +16,26 @@ function updateRemainingCharacters(event) {
 }
 
 productNameInputElement.addEventListener("input", updateRemainingCharacters);
+
+const spanElement = document.getElementById("remaining-chars");
+
+// spanElement.style.color = "red";
+// spanElement.style.fontSize = "30px";
+// We can access css properties by using .style
+// But the css property names in this JS code have camel case while css property names in CSS files has - marks.
+
+// spanElement.className = "first-class second-class"
+// This is how we set a class to an html element using JS.
+// In here, there are two classes created and added to the remaining-chars element which are first-class and second-class
+// Which means we can add more than one class to the same html element.
+
+// spanElement.className = "warning";
+// If there's already a class applied to the span element in the html code, then this code will "overwrite" that class
+// and create a class called .warning
+// so this code will overwrite some-class class and create a class called warning instead.
+
+spanElement.classList.add("warning");
+// this code will add a class called warning to the span element while still keeping the some-class class to.
+
+spanElement.classList.remove("warning");
+// this code will remove the .warning class form the span element.
