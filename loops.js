054bceb50024f646fd1e8aa1d29c18cda17310f1
Avishka Indula <avishkainduladev@gmail.com>
2022-05-11 13:13:48 +0530

    the regular for loop

---
+++ loops.js
@@ -0,0 +1,19 @@
+// Loops for arrays
+// for - Loops n number of times
+// for ... of - Loop through all elements of an array
+
+// Loops for objects
+// for ... in - Loop through all properties in an object
+// while - Loop as long as certain condition is met
+
+// --------------------------------------------------------------------------------------------------------
+// the regular for loop
+
+for (let i = 0; i < 10; i++) {
+  // i stands for iteration. But we can name it whatever we want.
+  // we use "let" instead of const here.
+  // (let i;)
+  // i++ means i = i + 1
+  // if we use i = i + 2 instead of i++ the value of i counts like 2,4,6,8
+  console.log(i);
+}
