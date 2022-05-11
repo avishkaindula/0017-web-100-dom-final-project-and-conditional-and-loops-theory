a6892aba8546b79771de8742b7377074a0bcd6cd
Avishka Indula <avishkainduladev@gmail.com>
2022-05-11 13:25:09 +0530

    the for-of loop

--- loops.js
+++ loops.js
@@ -17,3 +17,26 @@ for (let i = 0; i < 10; i++) {
   // if we use i = i + 2 instead of i++ the value of i counts like 2,4,6,8
   console.log(i);
 }
+
+// --------------------------------------------------------------------------------------------------------
+// the for-of loop
+
+const users = [
+  "Indula",
+  "Yashodha",
+  "Janith",
+  "Geethma",
+  "Janeesha",
+  "Shameen",
+];
+
+console.log(users);
+// This just console logs the array
+// but we want to console log the names inside that array as individual items.
+// for that we use for-of loop
+
+for (const userName of users) {
+  // we use const instead of let in here.
+  //   This for of loop get executed again and again for every element inside the array
+  console.log(userName);
+}
